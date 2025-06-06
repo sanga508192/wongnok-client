81658c3a0a2f2f304a848b549193e4fa8106de0a
SANGA_SISOT <62541094+sanga508192@users.noreply.github.com>
2025-06-07 00:56:08 +0700

    Update LoginPage.jsx

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -10,11 +10,12 @@ function LoginPage() {
 
   const handleLogin = async () => {
     try {
-      const res = await fetch("http://localhost:5000/api/auth/login", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ username, password }),
-      });
+const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ username, password }),
+});
+
 
       const data = await res.json();
 
