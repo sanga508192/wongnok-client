cbd96ea7593fc0a99dae2f44930ef7e849748083
SANGA_SISOT <62541094+sanga508192@users.noreply.github.com>
2025-06-07 00:57:48 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,11 +15,12 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://localhost:5000/api/auth/login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(form),
-    });
+const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/login`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({ username, password }),
+});
+
 
     const data = await res.json();
 
