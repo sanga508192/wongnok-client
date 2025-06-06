24f3d25efeb157e5fce916d8316bf1bd71455d1a
SANGA_SISOT <62541094+sanga508192@users.noreply.github.com>
2025-06-07 00:56:31 +0700

    Update Register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -13,11 +13,12 @@ function Register() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const res = await fetch("http://localhost:5000/api/auth/register", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(form),
-    });
+const res = await fetch(`${import.meta.env.VITE_API_URL}/api/auth/register`, {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify(form),
+});
+
 
     const data = await res.json();
 
